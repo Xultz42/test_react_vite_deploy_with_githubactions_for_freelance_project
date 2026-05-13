d93f6a911bf1b5541b70e261b5559a68456ba6a6
Henrique Schultz <henrique.schultz@dellteam.com>
2026-05-13 18:51:58 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="text-[#6fff00] font-mono font-bold text-3xl">
+          Funcionando
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
