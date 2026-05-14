555e66ac4fe8ecd0311ffef6ab99fb1c6fc3562b
Henrique Schultz <henrique.schultz@dellteam.com>
2026-05-14 10:19:40 -0300

    agoooora simmm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/test_react_vite_deploy_with_githubactions_for_freelance_project/",
 });
