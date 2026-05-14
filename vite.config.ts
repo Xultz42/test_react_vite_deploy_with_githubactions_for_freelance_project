4c0654133bb4116eaeb321871af0608e32260fab
Henrique Schultz <henrique.schultz@dellteam.com>
2026-05-14 09:13:24 -0300

    vamorfavoor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/test_react_vite_deploy_with_githubactions_for_freelance_project/",
 });
