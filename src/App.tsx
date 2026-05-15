22b6a236ff750198cf1bb1ce5bc0b85005d4c53d
Henrique-Schultz <valente.schultz@gmail.com>
2026-05-14 23:17:48 -0300

    exclamation point added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="text-[#6fff00] font-mono font-bold text-3xl">
-          Funcionando
+          Funcionando!
         </div>
       </div>
     </>
